033c7ed15e7913f48d00723de9daa94dbe3cc6cf
Transcendence <baydindenys@ukr.net>
2023-02-20 21:09:11 +0200

    h2 title fix
    h2 title fix

--- src/components/Statistic/Statistic.js
+++ src/components/Statistic/Statistic.js
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 export default function Statistics({ title, stats }) {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title && <h2 className={css.title}>{title}</h2>}
 
       <ul className={css.statList}>
         {stats.map((stat, index) => (
